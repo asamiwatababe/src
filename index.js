3c2c89db4e3ee0a2ed9312ff96f1b40a7ab01740
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2024-04-13 21:48:28 +0900

    要素をjsで作成して表示

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 
-{
-    function double(num){//num (引数)関数定義の定数は定義必要ない
-        return num * 2;
-    }
-    console.log(double(10));
-    console.log(double(4) * 3):
-}
+// {
+//     function double(num){//num (引数)関数定義の定数は定義必要ない
+//         return num * 2;
+//     }
+//     console.log(double(10));
+//     console.log(double(4) * 3):
+// }
+
+document.querySelector('button').addEventListener('click', () => {
+    const liElement = document.createElement('li');
+    liElement.textContent = 'hanako';
+    document.
+})
