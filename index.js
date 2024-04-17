ea1c5fd430905e62e3b3dc4be7a7ecb04d42d4c3
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2024-04-17 10:50:34 +0900

    checkboxの値にアクセス

--- index.js
+++ index.js
@@ -8,18 +8,18 @@
 //     console.log(double(4) * 3):
 // }
 {
-document.querySelector('button').addEventListener('click', () => {
-    const liElement = document.createElement('li');
-    liElement.textContent = 'Hanako';
-    // document.querySelector('ul').appendChild(liElement);
-    // okをクリックしたらjiroの前にhanako 
-    document.querySelector('ul').insertBefore(liElement,
-    document.querySelector('#second'));
-    // ボタンをクリックしたらjiroを削除
-    if(confirm('sure?') === true){
-    document.querySelector('#second').remove();
-    }
-});
+    document.querySelector('button').addEventListener('click', () => {
+        const liElement = document.createElement('li');
+        liElement.textContent = 'Hanako';
+        // document.querySelector('ul').appendChild(liElement);
+        // okをクリックしたらjiroの前にhanako 
+        document.querySelector('ul').insertBefore(liElement,
+            document.querySelector('#second'));
+        // ボタンをクリックしたらjiroを削除
+        if (confirm('sure?') === true) {
+            document.querySelector('#second').remove();
+        }
+    });
 }
 
 // フォーム部品の値にアクセスする方法
@@ -33,17 +33,31 @@ document.querySelector('button').addEventListener('click', () => {
 // Input,textareaの値にはvalueを使う
 
 // optionタグの中の選択した値がアラートで表示
-document.querySelector('button'),addEventListener('click',()=>{
+document.querySelector('button'), addEventListener('click', () => {
     alert(document.querySelector('select').value);
 });
 
 // radioボタンの使い方
 {
-    document.querySelector('button').addEventListener('click',() => {
+    document.querySelector('button').addEventListener('click', () => {
         document.querySelectorAll('input').forEach(() => {
-            if (radio.checked === true){
+            if (radio.checked === ture) {
                 alert(radio.value);
             }
-        })
+        });
     });
 }
+
+// checkboxの値にアクセス
+// 選択されたものを一括で表示
+document.querySelector('button').addEventListener('click', () => {
+    const colors = [];
+    document.querySelectorAll('input').forEach((checkbox) => {
+        if (checkbox.checked) {
+            // 空のcolorsの中にpushで値を追加していく
+            colors.push(checkbox.value)
+                // 配列を文字列にするにはjoinを使う
+                alert(colors.join(','));
+        }
+    });
+});
