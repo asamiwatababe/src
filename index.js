fbc59a427e2830a0aadc53f5d706640d318c5145
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2024-04-17 11:50:20 +0900

    focus,blurのイベント処理

--- index.js
+++ index.js
@@ -7,20 +7,20 @@
 //     console.log(double(10));
 //     console.log(double(4) * 3):
 // }
-{
-    document.querySelector('button').addEventListener('click', () => {
-        const liElement = document.createElement('li');
-        liElement.textContent = 'Hanako';
-        // document.querySelector('ul').appendChild(liElement);
-        // okをクリックしたらjiroの前にhanako 
-        document.querySelector('ul').insertBefore(liElement,
-            document.querySelector('#second'));
-        // ボタンをクリックしたらjiroを削除
-        if (confirm('sure?') === true) {
-            document.querySelector('#second').remove();
-        }
-    });
-}
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const liElement = document.createElement('li');
+//         liElement.textContent = 'Hanako';
+//         // document.querySelector('ul').appendChild(liElement);
+//         // okをクリックしたらjiroの前にhanako 
+//         document.querySelector('ul').insertBefore(liElement,
+//             document.querySelector('#second'));
+//         // ボタンをクリックしたらjiroを削除
+//         if (confirm('sure?') === true) {
+//             document.querySelector('#second').remove();
+//         }
+//     });
+// }
 
 // フォーム部品の値にアクセスする方法
 // inputの中に値を入力するとアラートで表示
@@ -51,23 +51,36 @@
 // checkboxの値にアクセス
 // 選択されたものを一括で表示
 {
-    document.querySelector('button').addEventListener('click',() => {
-        const colors = [];
-        document.querySelectorAll('input').forEach((checkbox) => {
-            if(checkbox.checked === true){
-                colors.push(checkbox.value);
-            }
-        });
-        alert(colors.join(','));
-    })
+    // document.querySelector('button').addEventListener('click',() => {
+    //     const colors = [];
+    //     document.querySelectorAll('input').forEach((checkbox) => {
+    //         if(checkbox.checked === true){
+    //             colors.push(checkbox.value);
+    //         }
+    //     });
+    //     alert(colors.join(','));
+    // })
 }
 
 // inputイベント inputに入力するとpタグにその値を反映
 // イベント指定→ text,textarea,input,select => input
 //             radio,checkbox => changeを使う
-document.querySelector('input').addEventListener('input', () => {
-    const p = document.querySelector('p');
-    const inputText = document.querySelector('input');
-    // inputText.valueだと文字列を反映し、inputText.value.lengthは文字数を反映
-    p.textContent = inputText.value.length;
-})
+// document.querySelector('input').addEventListener('input', () => {
+//     const p = document.querySelector('p');
+//     const inputText = document.querySelector('input');
+//     // inputText.valueだと文字列を反映し、inputText.value.lengthは文字数を反映
+//     p.textContent = inputText.value.length;
+// });
+
+// focus,blurイベント フォーカスされたときpタグにヒントが表示される
+{
+    document.querySelector('input').addEventListener('focus', () => {
+        document.querySelector('p').textContent = 'English only!';
+    });
+    // focusが外れる時のイベント処理 空文字にする
+    document.querySelector('input').addEventListener('blur', () => {
+        document.querySelector('p').textContent = '';
+    });
+
+    document.querySelector('input').focus();
+}
